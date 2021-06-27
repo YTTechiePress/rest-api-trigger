0eee84fa15ea2fe288d3dbdfd75a854b3da9dc41
Laurence Bahiirwa <laurence@mediauganda.com>
2021-06-27 08:23:07 +0300

    Issues

--- rest-api-trigger.php
+++ rest-api-trigger.php
@@ -10,3 +10,33 @@
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
  * text-domain: rest-api-trigger
 */
+
+defined( 'ABSPATH') or die( 'Authorized Access Denied!' );
+
+add_action( 'rest_api_init', 'techiepress_add_endpoint' );
+
+function techiepress_add_endpoint() {
+    register_rest_route(
+        'techiepress/trigger/v1',
+        'button-trigger',
+        array(
+            'method'              => 'GET',
+            'permission_callback' => '__return_true',
+            'callback'            => 'techiepress_info'
+        )
+    );
+}
+
+function techiepress_info() {
+    return 'Techiepress was here!';
+}
+
+add_action( 'wp_enqueue_scripts', 'techiepress_footer_js' );
+
+function techiepress_footer_js() {
+    wp_enqueue_script( 'trigger_ajax_js', plugin_dir_url(__FILE__) . '/js/main.js', array( 'jquery' ), '1.0.0', true );
+
+    wp_localize_script( 'trigger_ajax_js', 'rest_url', array(
+        'url' => esc_url_raw( rest_url( 'techiepress/trigger/v1/button-trigger' ) )
+    ) );
+}
