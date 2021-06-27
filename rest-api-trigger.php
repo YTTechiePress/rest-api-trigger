61f041008f708a1a38717c0d6117398904209b54
Laurence Bahiirwa <laurence@mediauganda.com>
2021-06-27 07:27:01 +0300

    Initial file

---
+++ rest-api-trigger.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Rest API trigger
+ * Plugin URI: https://github.com/yttechiepress/rest-api-trigger
+ * Author: Techiepress
+ * Author URI: https://github.com/yttechiepress/rest-api-trigger
+ * Description: Trigger a button using the REST API and Ajax.
+ * Version: 0.1.0
+ * License: GPL2
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: rest-api-trigger
+*/
